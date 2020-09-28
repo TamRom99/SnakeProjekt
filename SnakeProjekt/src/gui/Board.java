c1db4b9f3822704c7a808dc29f3e7ae90f8aaeb3
Tamara Romer <tamara.romer1@gmx.de>
2020-09-28 16:04:37 +0200

    Board nearly finished

--- SnakeProjekt/src/gui/Board.java
+++ SnakeProjekt/src/gui/Board.java
@@ -9,25 +9,26 @@ public class Board extends JLabel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		
-		//Draw bsckground
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
+		// Draw background
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(0, 0, Screen.width, Screen.height);
 		
 		
-		
-		g.setColor(Color.black);
-		for(int i =0; i < 16; i++) {
-			for(int k = 0; k < 16; i++) {
-				g.drawRect(i*32 + Screen.xoff, i*32 + Screen.yoff, 32, 32);
-				
-				
-		g.setColor(Color.black);
-		g.fillRect(Screen.xoff, Screen.yoff, 512, 512);
+		// Draw Grid
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(90, 70, 800, 640);
+		for (int i = 0; i < 25; i++) {
+			for (int k = 0; k < 20; k++) {
+				g.drawRect(i * 32 + Screen.xoff, k * 32 + Screen.yoff, 32, 32);
+
 			}
 		}
+		// Draw Border
+		g.setColor(Color.black);
+		g.drawRect(Screen.xoff, Screen.yoff, 800, 640);
+		repaint();
 	}
 
 }
--- SnakeProjekt/src/gui/Screen.java
+++ SnakeProjekt/src/gui/Screen.java
@@ -7,36 +7,37 @@ import javax.swing.*;
 
 
 public class Screen {
-	JFrame frame= new JFrame ();
+	JFrame frame;
 	
-	public static int height = 600;
-	public static int width = 800;
-	public static int xoff = 130;
-	public static int yoff=40; //20
+	public static int height = 850;
+	public static int width = 1000;
+	public static int xoff = 90;
+	public static int yoff= 70; //20
 	
 	public void createFrame() {
-		frame.setTitle("Snake");
+		frame = new JFrame ("The snake");
 		frame.setSize(width, height);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
 		frame.setLayout(null);
+		frame.setResizable(true);
+		
 		//frame.addKeyListener(new KeyHandler());
-		
-		JLabel captionLabel = new JLabel("Snake");
-		captionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		captionLabel.setFont(new Font("Cambria Math", Font.BOLD, 20));
-		captionLabel.setForeground(new Color(127, 255, 0));
-		captionLabel.setBackground(new Color(0, 0, 128));
-		captionLabel.setBounds(0, 0, 800, 32);
-		captionLabel.setOpaque(true);
-		frame.getContentPane().add(captionLabel);
-		
 		Board board = new Board();
 		board.setBounds(0, 0, width, height);
 		board.setVisible(true);
 		
-		frame.add(board);		
+		JLabel captionLabel = new JLabel("Snake");
+		captionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		captionLabel.setFont(new Font("Cambria Math", Font.BOLD, 30));
+		captionLabel.setForeground(new Color(127, 255, 0));
+		captionLabel.setBackground(new Color(0, 0, 128));
+		captionLabel.setBounds(0, 0, 1000, 52);
+		captionLabel.setOpaque(true);		
+		frame.getContentPane().add(captionLabel);		
+		frame.add(board);
+		
+		frame.requestFocus();
 		frame.setVisible(true);		
 		System.out.println("Window made");
 		
