3d53bf6210f1a44db15be5e6549480a4cad74169
Tamara Romer <tamara.romer1@gmx.de>
2020-09-30 17:25:57 +0200

    Update Food

--- SnakeProjekt/src/game/Food.java
+++ SnakeProjekt/src/game/Food.java
@@ -11,6 +11,12 @@ public class Food {
 		this.setFoodX((int) (Screen.xoff+ Math.random() * 25));
 		this.setFoodY((int) (Screen.yoff + Math.random() * 20));
 	}
+	
+	public void resetFood() {
+		this.setFoodX((int) (Screen.xoff+ Math.random() * 25));
+		this.setFoodY((int) (Screen.yoff + Math.random() * 20));
+	}
+
 
 	public int getFoodX() {
 		return FoodX;
