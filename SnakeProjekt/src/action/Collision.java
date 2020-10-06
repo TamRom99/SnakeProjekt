a1138f9da7520c35b140598455562eda73077ec8
alkan <alkan@DESKTOP-FPPLFNK.fritz.box>
2020-10-06 08:22:22 +0200

    Cross the Border

--- SnakeProjekt/src/action/Collision.java
+++ SnakeProjekt/src/action/Collision.java
@@ -12,7 +12,7 @@ public class Collision {
 			Snake.addTail();
 			Snake.food.placeFood();
 			score = score + 10;
-			Timer.millisec = Timer.millisec - 10;
+			Timer.millisec = Timer.millisec - 5;
 		}
 	}
 	public static boolean collideItself() {
@@ -25,8 +25,26 @@ public class Collision {
 		return false;
 	}
 
-	public static boolean collideBorder() {
-		return (Snake.head.getHeadX() < 0 || Snake.head.getHeadX() > Board.NumberOfBoxesX -1|| Snake.head.getHeadY() < 0
+	/*public static boolean collideBorder() {
+		return(Snake.head.getHeadX() < 0 || Snake.head.getHeadX() > Board.NumberOfBoxesX -1|| Snake.head.getHeadY() < 0
 				|| Snake.head.getHeadY() > Board.NumberOfBoxesY -1);
+			
 	}
+	*/
+	
+	public static boolean crossLeftBorder() {
+		return(Snake.head.getHeadX() < 0) ;
+	}
+	public static boolean crossRightBorder() {
+		return(Snake.head.getHeadX() > Board.NumberOfBoxesX -1) ;
+	}	
+	public static boolean crossUpperBorder() {
+		return(Snake.head.getHeadY() < 0) ;			
+	}		
+	public static boolean crossDownBorder() {
+		return(Snake.head.getHeadY() > Board.NumberOfBoxesY -1) ;
+			
+		}
+				
 }
+
